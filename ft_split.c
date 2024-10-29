06d0bb45df9785a34ba2531f7ffbe82454d03296
ihuseyin <borzslam@gmail.com>
2024-10-30 02:57:58 +0300

    Updated for Norminette rules

--- ft_split.c
+++ ft_split.c
@@ -18,13 +18,9 @@ static size_t	ft_count(const char *str, char c)
 
 static void	ft_free(char **result, size_t index)
 {
-	size_t	i;
-
-	i = 0;
-	while (i < index)
+	while (index--)
 	{
-		free(result[i]);
-		i++;
+		free(result[index]);
 	}
 	free(result);
 }
@@ -43,12 +39,24 @@ static char	*ft_strncpy(char *dest, const char *src, size_t n)
 	return (dest);
 }
 
+static int	ft_allocate(char **res, const char *srt, const char *end, size_t *i)
+{
+	res[*i] = malloc(end - srt + 1);
+	if (res[*i] == NULL)
+		return (0);
+	ft_strncpy(res[*i], srt, end - srt);
+	(*i)++;
+	return (1);
+}
+
 char	**ft_split(const char *s, char c)
 {
 	const char	*start;
 	char		**result;
 	size_t		index;
 
+	if (!s)
+		return (NULL);
 	result = malloc((ft_count(s, c) + 1) * sizeof(char *));
 	if (result == NULL)
 		return (NULL);
@@ -58,18 +66,14 @@ char	**ft_split(const char *s, char c)
 	{
 		if (*s == c)
 		{
-			result[index] = malloc(s - start + 1);
-			if (result[index] == NULL)
+			if (s > start && !ft_allocate(result, start, s, &index))
 				return (ft_free(result, index), NULL);
-			ft_strncpy(result[index++], start, s - start);
 			start = s + 1;
 		}
 		s++;
 	}
-	result[index] = malloc(s - start + 1);
-	if (result[index] == NULL)
+	if (s > start && !ft_allocate(result, start, s, &index))
 		return (ft_free(result, index), NULL);
-	ft_strncpy(result[index], start, s - start);
-	result[++index] = NULL;
+	result[index] = NULL;
 	return (result);
 }
