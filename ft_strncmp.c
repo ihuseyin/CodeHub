25dbbad0bc161d8bcbad4dbcb3cd27a934390bc5
ihuseyin <borzslam@gmail.com>
2024-10-30 13:00:50 +0300

    Update type casting

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -10,11 +10,11 @@ int	ft_strncmp(const char *s1, const char *s2, size_t len)
 	{
 		if (*s1 != *s2)
 		{
-			return (*s1 - *s2);
+			return (*(unsigned char *)s1 - *(unsigned char *)s2);
 		}
 		s1++;
 		s2++;
 		len--;
 	}
-	return (*s1 - *s2);
+	return (*(unsigned char *)s1 - *(unsigned char *)s2);
 }
