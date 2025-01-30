42d4a159e05751cc323a4472f1c88a5ee06457e4
ihuseyin <borzslam@gmail.com>
2025-01-30 18:45:35 +0300

    Update ft_memcpy.c

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -5,9 +5,11 @@ void	*ft_memcpy(void *dst, const void *src, size_t len)
 	const unsigned char	*src_ptr;
 	unsigned char		*dst_ptr;
 
+	if (!dst && !src)
+		return (0);
 	src_ptr = (const unsigned char *)src;
 	dst_ptr = (unsigned char *)dst;
-	while (len--)
+	while (len-- > 0)
 	{
 		*dst_ptr++ = *src_ptr++;
 	}
