aca054233bf06e30fab97bcd6387010c5bf80f77
ihuseyin <borzslam@gmail.com>
2025-01-30 18:34:04 +0300

    Update ft_strnstr.c

--- ft_strnstr.c
+++ ft_strnstr.c
@@ -2,24 +2,21 @@
 
 char	*ft_strnstr(const char *haystack, const char *needle, size_t len)
 {
-	size_t	needle_len;
 	size_t	i;
 	size_t	j;
 
-	if (!*needle)
+	if (needle[0] == '\0')
 		return ((char *)haystack);
-	needle_len = ft_strlen(needle);
-	if (needle_len > len)
-		return (NULL);
 	i = 0;
-	while (i <= len - needle_len)
+	while (haystack[i] != '\0' && i < len)
 	{
 		j = 0;
-		while (j < needle_len && haystack[i + j] == needle[j])
+		while (haystack[i + j] == needle[j] && (i + j) < len
+			&& needle[j] != '\0')
 		{
 			j++;
 		}
-		if (j == needle_len)
+		if (needle[j] == '\0')
 		{
 			return ((char *)&haystack[i]);
 		}
